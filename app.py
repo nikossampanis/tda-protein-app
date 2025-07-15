605b6e37d52ec14a7f2f5ab6e98de8ce8f332893
Nikolaos Sampanis <hwsampanis@gmail.com>
2025-07-15 10:41:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
+# 🧠 1. Όλα τα import πρώτα
 import streamlit as st
 from io import StringIO
 from utils.pdb_parser import extract_coordinates
 from utils.tda_pipeline import compute_persistence
 from utils.plot_diagram import plot_persistence_diagram
 
-# Ρυθμίσεις σελίδας
+# 🌐 2. Ρυθμίσεις σελίδας
 st.set_page_config(page_title="Protein TDA App", page_icon="🧬")
 
-# Τίτλος και υπογραφή
+# 🧾 3. Τίτλος και το όνομά σου
 st.title("Topological Analysis of Protein Structures")
 st.markdown("👨‍💻 **Developed by Nikos Sampanis**")
 
-# Πληροφορίες στο sidebar
+# 🗂️ 4. Sidebar
 st.sidebar.title("About")
 st.sidebar.markdown("🧬 **Protein TDA App**")
 st.sidebar.markdown("Developed by **Nikos Sampanis**")
-st.sidebar.markdown("[GitHub Repo](https://github.com/your-username/tda-protein-app)")
+st.sidebar.markdown("[GitHub Repo](https://github.com/nikossampanis/tda-protein-app)")
 
-# Ανεβάζουμε αρχείο
+# 📂 5. Upload αρχείου
 uploaded_file = st.file_uploader("Upload a .pdb file", type="pdb")
 if uploaded_file:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
@@ -26,4 +27,5 @@ if uploaded_file:
     st.write(f"Loaded {len(coords)} atoms.")
     diagram = compute_persistence(coords)
     st.pyplot(plot_persistence_diagram(diagram))
+
 
