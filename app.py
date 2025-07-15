0910bd05e9c97aa28d8a9cd1163e10ea9bcb9ee0
Nikolaos Sampanis <hwsampanis@gmail.com>
2025-07-15 10:36:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,20 @@ from utils.pdb_parser import extract_coordinates
 from utils.tda_pipeline import compute_persistence
 from utils.plot_diagram import plot_persistence_diagram
 
+# Ρυθμίσεις σελίδας
 st.set_page_config(page_title="Protein TDA App", page_icon="🧬")
 
+# Τίτλος και υπογραφή
 st.title("Topological Analysis of Protein Structures")
-st.markdown("**Developed by Nikos Sampanis**")
+st.markdown("👨‍💻 **Developed by Nikos Sampanis**")
 
+# Πληροφορίες στο sidebar
 st.sidebar.title("About")
 st.sidebar.markdown("🧬 **Protein TDA App**")
 st.sidebar.markdown("Developed by **Nikos Sampanis**")
 st.sidebar.markdown("[GitHub Repo](https://github.com/your-username/tda-protein-app)")
 
+# Ανεβάζουμε αρχείο
 uploaded_file = st.file_uploader("Upload a .pdb file", type="pdb")
 if uploaded_file:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
